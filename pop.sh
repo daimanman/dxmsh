1eff12cd7527141f96fefee3f81518fb9c130570
daixiaoman <2821224626@qq.com>
2018-10-09 10:03:37 +0800

    zmm pop.sh

---
+++ pop.sh
@@ -0,0 +1,11 @@
+dir=.
+for file in $dir/*;
+do
+  if [ "$file" != "./pop.sh" ]
+  then
+  start=$(awk '{if($0 ~ /Inferred clusters/){print NR;exit;}}' $file )
+  end=$(awk '{if($0 ~ /Estimated Allele Frequencies/){print NR;exit;}}' $file )
+  awk -v start=$start -v end=$end '{if(NR > start && NR < end){print $0}}' $file
+
+  fi
+done

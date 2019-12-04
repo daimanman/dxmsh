0c78b7c242de312c71e63db099ade2d5c9970a55
daixiaoman <2821224626@qq.com>
2019-12-04 08:51:43 +0800

    ok

--- pop.sh
+++ pop.sh
@@ -5,7 +5,7 @@ do
   then
   start=$(awk '{if($0 ~ /Inferred clusters/){print NR;exit;}}' $file )
   end=$(awk '{if($0 ~ /Estimated Allele Frequencies/){print NR;exit;}}' $file )
-  awk -v start=$start -v end=$end '{if(NR > start && NR < end){print $0}}' $file
+  awk -v start=$start -v end=$end '{if(NR > start && NR < end){print $0;if(NR > end){exit;}}}' $file
 
   fi
 done
